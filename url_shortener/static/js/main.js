28fc25fa89f6e7b8099655780ef8bed939c9211c
calin.bule@gmail.com <Rogvaiv3#>
2020-05-04 00:40:01 +0200

    first commit; functional app'

---
+++ url_shortener/static/js/main.js
@@ -0,0 +1,55 @@
+function copyLink() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+
+function validateForm() {
+  var x = document.forms["linkForm"]["original_url"].value;
+  if (x == "") {
+    alert("The original link field must be filled out");
+    return false;
+  } else if (x.toLowerCase().includes("http://") == false &&  x.toLowerCase().includes("https://") == false) {
+    alert("For the redirection to work correctly you also have to include the http:// or https:// part of your link. Please do so and then try again.");
+    return false;
+    }
+}
+
